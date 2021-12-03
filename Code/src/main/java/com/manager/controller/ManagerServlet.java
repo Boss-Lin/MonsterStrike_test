3129280fbcfdbb4073a42cd9e24a7836f88e011b
DESKTOP-KKB0FOS\翔翔好棒棒 <bbosstw@gmail.com>
2021-12-03 18:38:05 +0800

    新增@Controller的物件導向

--- Code/src/main/java/com/manager/controller/ManagerServlet.java
+++ Code/src/main/java/com/manager/controller/ManagerServlet.java
@@ -1,11 +1,14 @@
 package com.manager.controller;
 
+import org.springframework.stereotype.Controller;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Controller
 public class ManagerServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         doPost(req, res);
